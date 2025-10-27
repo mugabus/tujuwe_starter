66c58ede70b719b12fb6f08033fab42f9824caca
mugabus <fatakimugaruka91@gmail.com>
2025-10-27 07:00:33 -0700

    restrucrure the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
-import 'screens/main_page.dart';
-import 'screens/details_screen.dart';
+import 'presentation/theme/app_colors.dart';
+import 'presentation/screens/splash/splash_screen.dart';
+import 'presentation/screens/main_page.dart';
+import 'presentation/state/content_provider.dart';
+import 'data/datasources/remote/api_service.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const TujuweApp());
@@ -12,26 +15,25 @@ class TujuweApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TUJUWE',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Roboto',
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFF0D47A1),
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
-            .copyWith(secondary: Colors.blueAccent),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF0D47A1),
-          foregroundColor: Colors.white,
-          elevation: 0,
+    final api = ApiService();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ContentProvider(api)),
+      ],
+      child: MaterialApp(
+        title: 'TUJUWE',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: AppColors.primary,
+          scaffoldBackgroundColor: AppColors.background,
+          appBarTheme: const AppBarTheme(backgroundColor: AppColors.primary, foregroundColor: Colors.white),
         ),
+        initialRoute: '/',
+        routes: {
+          '/': (_) => const SplashScreen(),
+          '/home': (_) => const MainPage(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (_) => const SplashScreen(),
-        '/home': (_) => const MainPage(),
-      },
     );
   }
 }
